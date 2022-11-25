c4154b694192a4dd71904bd390712136a349a559
Watkins <sian.watkins@accenture.com>
2022-11-25 12:17:54 +0000

    new changes

--- exercises/Exercises3/problems-8.js
+++ exercises/Exercises3/problems-8.js
@@ -9,7 +9,22 @@
 */
 
 function react(sequence) {
-
+    sequence = sequence.toLowerCase()
+    sequence = sequence.split('');
+    for(let i = 0; i < sequence.length; i++){
+        let j = i + 1 
+        if (j < sequence.length - 1){
+            if (sequence[i] == sequence [j]){
+                removedElement = sequence.splice(i, 1)
+                removedElement = sequence.splice(i, 1)
+            }
+        }else{
+            i = -1;
+            j = 1;
+        }
+    }
+    sequence=sequence.join("");
+    console.log(sequence)
 }
 
 // Do not modify this code
